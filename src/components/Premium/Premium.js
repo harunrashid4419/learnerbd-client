b5ec9cb5a49c81a36cecceeb58794163336a5aca
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-25 21:37:51 +0600

    premium complited

--- src/components/Premium/Premium.js
+++ src/components/Premium/Premium.js
@@ -1,8 +1,8 @@
 import React, { useContext } from "react";
-import { Button, Container, Image } from "react-bootstrap";
+import { Button, ButtonGroup, Container, Form, Image } from "react-bootstrap";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Context/UserContext";
-import './Premium.css';
+import "./Premium.css";
 
 const Premium = () => {
    const courseDetails = useLoaderData();
@@ -12,24 +12,8 @@ const Premium = () => {
    return (
       <div className="py-5 bg-secondary">
          <Container>
-            {/* <div className="ms-5 mb-4">
-               <h3 className="text-dark">
-                  Course ID:{" "}
-                  <span className="text-danger">{courseDetails.id}</span>
-               </h3>
-               <h5 className="text-dark py-2">
-                  Course Name:{" "}
-                  <span className="text-danger">{courseDetails.name}</span>
-               </h5>
-               <p>{courseDetails.title}</p>
-               <p>{courseDetails.details}</p>
-            </div>
-            <img src={courseDetails.image} alt="" />
-            <div className="mt-5 text-center">
-               <Link to='/blog'><Button variant="primary">Read Blog</Button></Link>
-            </div> */}
             <div className="d-flex justify-content-between align-items-center">
-               <h3 className="text-white">ID: {courseDetails.id}</h3>
+               <h3 className="text-white">Course ID: {courseDetails.id}</h3>
                <Image
                   style={{ width: "70px" }}
                   className="ms-3 rounded-circle"
@@ -37,14 +21,80 @@ const Premium = () => {
                   alt=""
                />
             </div>
-            {/* <div className="form-section">
-               <h3 className="text-white text-center mt-3 mb-4">User's check out form</h3>
-               <input className="name" type="text" name="name" value="" placeholder="Enter your Name"/>
-               <input className="email" type="email" name="name" value="" placeholder="Enter your Email"/>
-               <input className="city" type="text" name="city" value="" placeholder="Enter your City"/>
-               <input className="country" type="text" name="country" value="" placeholder="Enter your Country"/>
-               <input className="number" type="number" name="phone" value="" placeholder="Enter your Phone"/>
-            </div> */}
+            <Form className="form">
+               <h3 className="text-center text-white mb-3">
+                  User's check out form
+               </h3>
+               <div id="course-info" className="d-flex justify-content-between">
+                  <h4 className="text-white mb-4">
+                     Course Name: <span>{courseDetails?.name}</span>
+                  </h4>
+                  <h4 className="text-white mb-4">
+                     Price: <span>${courseDetails?.price}</span>
+                  </h4>
+               </div>
+               <Form.Group className="mb-4" controlId="formBasicText">
+                  <Form.Control
+                     readOnly
+                     defaultValue={user?.displayName}
+                     type="text"
+                     placeholder="Enter Name"
+                  />
+               </Form.Group>
+               <Form.Group className="mb-4" controlId="formBasicEmail">
+                  <Form.Control
+                     defaultValue={user?.email}
+                     type="email"
+                     placeholder="Enter email"
+                  />
+               </Form.Group>
+
+               <div className="d-flex">
+                  <Form.Group
+                     className="mb-4 address"
+                     controlId="formBasicText2"
+                  >
+                     <Form.Control
+                        type="text"
+                        placeholder=" Enter your Address"
+                     />
+                  </Form.Group>
+                  <Form.Group className="mb-4 city" controlId="formBasicCity">
+                     <Form.Control
+                        type="text"
+                        placeholder=" Enter phone city"
+                     />
+                  </Form.Group>
+               </div>
+
+               <div className="d-flex">
+                  <Form.Group
+                     className="mb-4 country"
+                     controlId="formBasicText2"
+                  >
+                     <Form.Control
+                        type="text"
+                        placeholder=" Enter your country"
+                     />
+                  </Form.Group>
+                  <Form.Group
+                     className="mb-4 number"
+                     controlId="formBasicNumber"
+                  >
+                     <Form.Control
+                        type="number"
+                        placeholder=" Enter phone number"
+                     />
+                  </Form.Group>
+               </div>
+               <div className="text-center mt-3">
+                  <Link>
+                     <Button className="process-btn">
+                        Process
+                     </Button>
+                  </Link>
+               </div>
+            </Form>
          </Container>
       </div>
    );
