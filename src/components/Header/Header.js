d8d8f95c1918a6b8c7928aaf3ac1347b3baf509e
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-25 23:42:46 +0600

    top header add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { Button, Container, Image } from "react-bootstrap";
 import { AuthContext } from "../../Context/UserContext";
 import { FaUser } from "react-icons/fa";
+import TopHeader from "../TopHeader/TopHeader";
 
 const Header = () => {
    const { user, logOut } = useContext(AuthContext);
@@ -16,44 +17,50 @@ const Header = () => {
    };
 
    return (
-      <div className="main-header">
-         <Container>
-            <nav className="header">
-               <div className="left">
-                  <Link to="/">FreeLearn</Link>
-               </div>
-               <div className="menu">
-                  <Link to="/home">Home</Link>
-                  <Link to="/courses">Courses</Link>
-                  <Link to="/blog">Blog</Link>
-                  {user ? (
-                     <>
-                        <Button onClick={handleLogOut} variant="outline-danger">
-                           LogOut
-                        </Button>
-                        {user?.photoURL ? (
-                           <Image
-                              style={{ width: "40px" }}
-                              className="ms-3 rounded-circle"
-                              src={user?.photoURL}
-                              alt=""
-                           />
-                        ) : (
-                           <FaUser
-                              className="ms-3 text-white"
-                              style={{ fontSize: "35px" }}
-                           />
-                        )}
-                     </>
-                  ) : (
-                     <>
-                        <Link to="/registration">Registration</Link>
-                        <Link to="/login">LogIn</Link>
-                     </>
-                  )}
-               </div>
-            </nav>
-         </Container>
+      <div>
+         <TopHeader></TopHeader>
+         <div className="main-header">
+            <Container>
+               <nav className="header">
+                  <div className="left">
+                     <Link to="/">FreeLearn</Link>
+                  </div>
+                  <div className="menu">
+                     <Link to="/home">Home</Link>
+                     <Link to="/courses">Courses</Link>
+                     <Link to="/blog">Blog</Link>
+                     {user ? (
+                        <>
+                           <Button
+                              onClick={handleLogOut}
+                              variant="outline-danger"
+                           >
+                              LogOut
+                           </Button>
+                           {user?.photoURL ? (
+                              <Image
+                                 style={{ width: "40px" }}
+                                 className="ms-3 rounded-circle"
+                                 src={user?.photoURL}
+                                 alt=""
+                              />
+                           ) : (
+                              <FaUser
+                                 className="ms-3 text-white"
+                                 style={{ fontSize: "35px" }}
+                              />
+                           )}
+                        </>
+                     ) : (
+                        <>
+                           <Link to="/registration">Registration</Link>
+                           <Link to="/login">LogIn</Link>
+                        </>
+                     )}
+                  </div>
+               </nav>
+            </Container>
+         </div>
       </div>
    );
 };
---
+++ src/components/TopHeader/TopHeader.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./TopHeader.css";
+import {
+   FaFacebook,
+   FaInstagram,
+   FaMailBulk,
+   FaPhoneAlt,
+   FaTwitter,
+   FaYoutube,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+const TopHeader = () => {
+   return (
+      <div className="header-bg">
+         <div className="top-header container">
+            <div className="info">
+               <p>
+                  <FaMailBulk /> harunrashid4419@gmail.com
+               </p>
+               <p>
+                  <FaPhoneAlt /> 01185251500
+               </p>
+            </div>
+            <div className="social-icon">
+               <div>
+                  <Link>
+                     <FaFacebook />
+                  </Link>
+                  <Link>
+                     <FaTwitter />
+                  </Link>
+                  <Link>
+                     <FaInstagram />
+                  </Link>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
+};
+
+export default TopHeader;
