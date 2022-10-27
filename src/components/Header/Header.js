6bda364ed51e308a2b87f71d5c2d93de05323fc7
harunorrashid4419 <harunrashid4419@gmail.com>
2022-10-27 14:52:10 +0600

    active router add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,14 @@
 import React, { useContext, useState } from "react";
 import "./Header.css";
-import { Link } from "react-router-dom";
-import { Button, Container, Image } from "react-bootstrap";
+import { Link, NavLink } from "react-router-dom";
+import {
+   Button,
+   Container,
+   Image,
+   Nav,
+   Navbar,
+   NavDropdown,
+} from "react-bootstrap";
 import { AuthContext } from "../../Context/UserContext";
 import { FaBars, FaTimes, FaUser } from "react-icons/fa";
 import TopHeader from "../TopHeader/TopHeader";
@@ -21,60 +28,100 @@ const Header = () => {
    return (
       <div>
          <TopHeader></TopHeader>
-         <div className="main-header">
+         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-               <nav className="header">
-                  <div className="left">
-                     <Link to="/">FreeLearn</Link>
-                  </div>
-                  <div className="menu">
-                     <Link to="/home">Home</Link>
-                     <Link to="/courses">Courses</Link>
-                     <Link to="/blog">Blog</Link>
-
-                     {user ? (
-                        <>
-                           <Button
-                              onClick={handleLogOut}
-                              variant="outline-danger"
-                           >
-                              <ReactTooltip />
-                              LogOut
-                           </Button>
-                           {user?.photoURL ? (
-                              <Image
-                                 data-tip={user?.displayName}
-                                 style={{ width: "40px" }}
-                                 className="ms-3 rounded-circle"
-                                 src={user?.photoURL}
-                                 alt=""
-                              />
-                           ) : (
-                              <FaUser
-                                 data-tip={user?.displayName}
-                                 className="ms-3 text-white"
-                                 style={{ fontSize: "35px" }}
-                              />
-                           )}
-                        </>
-                     ) : (
-                        <>
-                           <Link to="/registration">Registration</Link>
-                           <Link to="/login">LogIn</Link>
-                           <ReactTooltip />
-                        </>
-                     )}
-                     <span className="ms-2" onClick= {() => setDark(!dark)}>
+               <Link className="left-side" to="/">
+                  FreeLearn
+               </Link>
+               <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+               <Navbar.Collapse id="responsive-navbar-nav">
+                  <Nav className="me-auto">
+                     <span className="ms-3" onClick={() => setDark(!dark)}>
                         {dark ? (
                            <Button variant="light">Light</Button>
-                           ) : (
+                        ) : (
                            <Button variant="dark">Dark</Button>
                         )}
                      </span>
-                  </div>
-               </nav>
+                  </Nav>
+                  <Nav>
+                     <div className="menu">
+                        <NavLink
+                           className={({ isActive }) =>
+                              isActive ? "text-danger" : undefined
+                           }
+                           to="/home"
+                        >
+                           Home
+                        </NavLink>
+                        <NavLink
+                           className={({ isActive }) =>
+                              isActive ? "text-danger" : undefined
+                           }
+                           to="/courses"
+                        >
+                           Courses
+                        </NavLink>
+                        <NavLink
+                           className={({ isActive }) =>
+                              isActive ? "text-danger" : undefined
+                           }
+                           to="/blog"
+                        >
+                           Blog
+                        </NavLink>
+
+                        {user ? (
+                           <>
+                              <Button
+                                 onClick={handleLogOut}
+                                 variant="outline-danger"
+                              >
+                                 <ReactTooltip />
+                                 LogOut
+                              </Button>
+                              {user?.photoURL ? (
+                                 <Image
+                                    data-tip={user?.displayName}
+                                    style={{ width: "40px" }}
+                                    className="ms-3 rounded-circle"
+                                    src={user?.photoURL}
+                                    alt=""
+                                 />
+                              ) : (
+                                 <FaUser
+                                    data-tip={user?.displayName}
+                                    className="ms-3 text-white"
+                                    style={{ fontSize: "35px" }}
+                                 />
+                              )}
+                           </>
+                        ) : (
+                           <>
+                              <NavLink
+                                 className={({ isActive }) =>
+                                    isActive ? "text-danger" : undefined
+                                 }
+                                 to="/registration"
+                              >
+                                 Registration
+                              </NavLink>
+                              <NavLink
+                                 className={({ isActive }) =>
+                                    isActive ? "text-danger" : undefined
+                                 }
+                                 to="/login"
+                              >
+                                 LogIn
+                              </NavLink>
+                              <ReactTooltip />
+                           </>
+                        )}
+                     </div>
+                  </Nav>
+               </Navbar.Collapse>
             </Container>
-         </div>
+         </Navbar>
       </div>
    );
 };
